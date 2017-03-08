3f5fcd68866eb08b83a1e51ff98c7e1dbb6608a6
Brit Butler <redline6561@gmail.com>
2017-03-08 12:00:11 -0500

    Add a controller method to create a shelter.

--- server/controllers/shelter.js
+++ server/controllers/shelter.js
@@ -1,5 +1,12 @@
+const Shelter = require("../models").Shelter;
+
 module.exports = {
   create (req, res) {
-    res.status(200).send("Hello world!");
+    Shelter.create({
+      name: req.body.name,
+      location: req.body.location
+    })
+      .then(shelter => res.status(201).send(shelter))
+      .catch(error => res.status(400).send(error));
   }
 };
