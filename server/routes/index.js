19db55588e8cdb20d47b92a52715eaf59cc0bf43
Brit Butler <redline6561@gmail.com>
2017-03-08 11:52:37 -0500

    Get a route and controller up and running.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 // Require your controllers here
-
+const ShelterController = require("../controllers/shelter");
 
 module.exports = (app) => {
   // Add your routes here
+  app.post('/shelters', ShelterController.create);
+
 };
